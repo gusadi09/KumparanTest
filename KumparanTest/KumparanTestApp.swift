d81184997f9bab470d0b543febf18acc3b15cd40
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-02-03 07:36:28 +0700

    Initial Commit

---
+++ KumparanTest/KumparanTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KumparanTestApp.swift
+//  KumparanTest
+//
+//  Created by Gus Adi on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct KumparanTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
