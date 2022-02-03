db5f705a8c0280db53ff9fa4dad6221f8e5ce08e
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-02-03 08:20:44 +0700

    [iOS-KT001] make reusable target type

--- KumparanTest/Common/Data/Reusables/TargetType/KumparanTestTargetType.swift
+++ KumparanTest/Common/Data/Reusables/TargetType/KumparanTestTargetType.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Moya
+
+protocol KumparanTestTargetType: TargetType {
+	var parameters: [String: Any] {
+		get
+	}
+}
+
+extension KumparanTestTargetType {
+	var baseURL: URL {
+		return URL(string: "https://jsonplaceholder.typicode.com") ?? (NSURL() as URL)
+	}
+
+	var parameterEncoding: Moya.ParameterEncoding {
+		JSONEncoding.default
+	}
+
+	var task: Task {
+		return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+	}
+
+	var headers: [String: String]? {
+		return [:]
+	}
+}
