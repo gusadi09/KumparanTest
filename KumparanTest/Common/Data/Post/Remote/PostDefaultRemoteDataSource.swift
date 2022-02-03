cc5f8a1816cfddfb415410210d4b2b3c5a714ecc
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-02-03 23:29:22 +0700

    [iOS-KT003] make target type

---
+++ KumparanTest/Common/Data/Post/Remote/PostDefaultRemoteDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  PostDefaultRemoteDataSource.swift
+//  KumparanTest
+//
+//  Created by Gus Adi on 03/02/22.
+//
+
+import Foundation
---
+++ KumparanTest/Common/Data/Post/Remote/PostRemoteDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  PostRemoteDataSource.swift
+//  KumparanTest
+//
+//  Created by Gus Adi on 03/02/22.
+//
+
+import Foundation
