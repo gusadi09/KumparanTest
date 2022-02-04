4c3d1788a0762b56427b844dd11086085fd733fc
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-02-04 07:53:01 +0700

    [iOS-KT004] make post detail header and body

--- KumparanTest/DetailPost/View/DetailPostView.swift
+++ KumparanTest/DetailPost/View/DetailPostView.swift
@@ -9,8 +9,64 @@ import SwiftUI
 
 struct DetailPostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+		ScrollView(.vertical, showsIndicators: true) {
+			VStack(spacing: 30) {
+				PostHeader()
+
+				PostBody()
+			}
+		}
+		.navigationTitle("Post Detail")
+	}
+}
+
+extension DetailPostView {
+
+	struct PostHeader: View {
+		var body: some View {
+			HStack {
+				VStack(alignment: .leading, spacing: 15) {
+
+					Text("unt aut facere repellat provident occaecati excepturi optio reprehenderit")
+
+					Button {
+
+					} label: {
+						HStack {
+							Circle()
+								.scaledToFit()
+								.frame(height: 40)
+								.foregroundColor(.gray)
+
+							Text("Bret")
+								.underline()
+						}
+					}
+
+				}
+
+				Spacer()
+			}
+
+		}
+	}
+
+	struct PostBody: View {
+		var body: some View {
+			HStack {
+				VStack(alignment: .leading, spacing: 10) {
+					Text("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")
+						.multilineTextAlignment(.leading)
+
+				}
+
+				Spacer()
+
+			}
+		}
+	}
+
+	
 }
 
 struct DetailPostView_Previews: PreviewProvider {
