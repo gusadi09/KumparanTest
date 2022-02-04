f81afedf09dfadc469e698778e3fa80f096a8b80
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-02-04 07:35:47 +0700

    [iOS-KT004] initiate first detail post file structure

---
+++ KumparanTest/DetailPost/View/DetailPostView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailPostView.swift
+//  KumparanTest
+//
+//  Created by Gus Adi on 04/02/22.
+//
+
+import SwiftUI
+
+struct DetailPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPostView()
+    }
+}
---
+++ KumparanTest/DetailPost/ViewModel/DetailPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPostViewModel.swift
+//  KumparanTest
+//
+//  Created by Gus Adi on 04/02/22.
+//
+
+import Foundation
